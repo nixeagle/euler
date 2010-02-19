9b69156e57a5b7a7687bbcaf01ab0b695001f19e
U-awned-PC\awned <awned@awned-PC.(none)>
2010-02-18 21:28:32 -0500

    my solution

---
+++ 1/awned.pas
@@ -0,0 +1,19 @@
+program sumuthou(output);
+
+var i : integer;
+    ret : real;
+
+const top = 1000;
+      bottom = 1;
+
+begin
+    ret := 0;
+
+    for i := 1 to 999 do begin
+        if ((i mod 3) = 0) or ((i mod 5) = 0) then
+            ret := ret + i;
+    end;
+
+    writeln (ret:2:2);
+
+end.
