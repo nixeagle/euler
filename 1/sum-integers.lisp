4175a51d9ca5dc07b655a8add1a8afa750939a65
James S <i@nixeagle.org>
2010-02-19 06:56:22 +0000

    change SUM-INTEGERS-IF to SUM-INTEGERS making the test and step keywords.

--- 1/sum-integers.lisp
+++ 1/sum-integers.lisp
@@ -1,9 +1,9 @@
 (in-package :nisp.euler)
 
-(defun sum-integers-if (predicate start end &optional (step 1))
+(defun sum-integers (start end &key (test #'=) (step 1))
   "Sum numbers satisfying PREDICATE from START to END."
   (iterate (for n :from start :to end :by step)
-           (when (funcall predicate n)
+           (when (funcall test n)
              (sum n :into result))
            (declare (fixnum result))    ; Help the compiler optimize.
            (finally (return result))))
@@ -12,12 +12,11 @@
   "Call `sum-integers-if' after we compose predicate.
 
 This is a purely functional way of solving this problem."
-  (sum-integers-if (disjoin (compose (curry #'= 0) (rcurry #'mod 3))
-                            (compose (curry #'= 0) (rcurry #'mod 5)))
-                   0 999))
+  (sum-integers 0 999 :test (disjoin (compose (curry #'= 0) (rcurry #'mod 3))
+                            (compose (curry #'= 0) (rcurry #'mod 5)))))
 
 (defun solution-1/2 ()
   "Use a local function definition for the predicate."
   (flet ((validp (n)
            (or (= 0 (mod n 3)) (= 0 (mod n 5)))))
-    (sum-integers-if #'validp 0 999)))
+    (sum-integers 0 999 :test #'validp)))
