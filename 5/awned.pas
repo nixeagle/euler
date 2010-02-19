098d4c5fb2d70a0e62c704e0f7a5b1ca1d801d21
U-awned-PC\awned <awned@awned-PC.(none)>
2010-02-19 18:50:47 -0500

    algorith still doesnt work but at least it runs

--- 5/awned.pas
+++ 5/awned.pas
@@ -11,9 +11,12 @@ begin
 
     x := 0;
 
-    while (a[1] <> 0) and (a[2] <> 0) and (a[3] <> 0) and (a[4] <> 0) do begin
+    while (a[1] <> 0) and (a[2] <> 0) and (a[3] <> 0) and (a[4] <> 0) and  (a[5] <> 0) and (a[6] <> 0) and (a[7] <> 0) and (a[8] <> 0) and (a[9] <> 0) and (a[10] <> 0) and (a[11] <> 0) and (a[12] <> 0) and (a[13] <> 0) and (a[14] <> 0) and (a[15] <> 0) and (a[16] <> 0) and (a[17] <> 0) and (a[18] <> 0) and (a[19] <> 0) and (a[20] <> 0) do begin
         x := x + 1;
-        for i := 1 to 20 do
-            a[i] = x div i
+        for i := 1 to 20 do begin
+            a[i] := x div i;
+        end;
     end;
+    writeln (x);
 end.
+
