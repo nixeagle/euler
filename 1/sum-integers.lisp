cde24772ef54c2238f5b2d0ad230e13260af7523
James S <i@nixeagle.org>
2010-02-19 21:40:30 +0000

    A working and correct SUM-INTEGERS-RECURSIVE

--- 1/sum-integers.lisp
+++ 1/sum-integers.lisp
@@ -25,13 +25,28 @@ This is a purely functional way of solving this problem."
 
 (defun sum-integers-recursive
     (current end &key (total 0) (step 1) (test #'integerp))
+  "Sum numbers satisfying TEST from CURRENT to END."
   (declare (optimize (safety 0) (debug 0) (speed 3) (space 0)
                      (compilation-speed 0))
            (type fixnum current end step)
-           (type integer total))
-  (if (< current end)
-      (sum-integers2 (1+ current) end :step step :test test
+           (type integer total)
+           (type function test))
+  (if (<= current end)
+      (sum-integers-recursive (the fixnum (1+ current)) end :step step :test test
                      :total (if (funcall test current)
-                                (+ total current)
+                                (the integer (+ total current))
                                 total))
       total))
+
+(defun solution-1/3 ()
+  "recursive form"
+  (sum-integers-recursive
+   0 999
+   :test (disjoin (compose (curry #'= 0) (rcurry #'mod 3))
+                  (compose (curry #'= 0) (rcurry #'mod 5)))))
+
+(defun solution-1/4 ()
+  "recursive form with local function def"
+  (flet ((validp (n)
+           (or (= 0 (mod n 3)) (= 0 (mod n 5)))))
+    (sum-integers-recursive 0 999 :test #'validp)))
