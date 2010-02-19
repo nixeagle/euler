778d6267550331fffc509300230bf66b15b7c4ab
U-awned-PC\awned <awned@awned-PC.(none)>
2010-02-19 07:00:30 -0500

    my solution

--- 2/awned.pas
+++ 2/awned.pas
@@ -1,7 +1,8 @@
 {Alexandre Marcondes - Project Euler - Problem 2}
 program p2(output);
 
-var x, y, z, ret : real;
+var x, y, z : longint;
+    ret : longint;
 
 const top = 4000000;
 
@@ -13,15 +14,29 @@ begin
 
     while (x < top) and (y < top) and (z < top) do begin
         z := x + y;
+        
+        if (z > top) then
+            break;
+
         if ((z mod 2) = 0) then
             ret := ret + z;
+    
         x := z + y;
+
+        if (x > top) then
+            break;
+        
         if ((x mod 2) = 0) then
             ret := ret + x;
+        
         y := x + z;
+
+        if (y > top) then
+            break;
+    
         if ((y mod 2) = 0) then
             ret := ret + y;
     end;
 
-    writeln (ret:2:2);
+    writeln (ret);
 end.
