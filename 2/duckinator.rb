27d864b6a13a8fe80fb45b355ac92f84dea01020
Nick Markwell <nick@duckinator.net>
2010-02-19 21:05:45 -0500

    Added my ruby version of #1

---
+++ 2/duckinator.rb
@@ -0,0 +1,27 @@
+def foo
+vals = [1, 2]
+
+limit = 4_000_000
+
+ret = 0
+
+if vals[0] % 2 == 0
+  ret += vals[0]
+end
+
+if vals[1] % 2 == 0
+  ret += vals[1]
+end
+
+i = vals[1]
+new = 0
+while new <= limit
+  new = vals[0]+vals[1]
+  vals = [vals[1], new]
+  if vals[1] % 2 == 0
+    ret += new
+  end
+end
+puts ret
+
+end
