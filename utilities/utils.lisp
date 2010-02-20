29444e9973b4db27a93dd1f7b2e6eace83753735
James S <i@nixeagle.org>
2010-02-20 17:22:36 +0000

    Add COLLATZ-SEQUENCE

--- utilities/utils.lisp
+++ utilities/utils.lisp
@@ -9,7 +9,16 @@ Something like 123 becomes (1 2 3)."))
 (defmethod split-integer ((input integer))
   (parse-integers (princ-to-string input)))
 
-
 (defun expt-self (integer)
   "INTEGER^INTEGER"
   (expt integer integer))
+
+;;; Not yet recursively defined ;)
+(defun collatz-sequence (positive-fixnum)
+  (declare (type positive-fixnum positive-fixnum))
+  (iter (with n = positive-fixnum)
+        (collect n)
+        (until (= n 1))
+        (if (evenp n)
+            (setq n (/ n 2))
+            (setq n (+ (* 3 n) 1)))))
